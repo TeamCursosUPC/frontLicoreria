272962ea00dbaf6953cd14100f623e0a6cfdc1cb
Alan Cornejo <alancornejo@icloud.com>
2017-11-20 01:24:00 -0500

    Mejoras en el template layout

--- resources/views/layout/layout-admin.blade.php
+++ resources/views/layout/layout-admin.blade.php
@@ -8,13 +8,14 @@
     <meta name="_token" content="{{ csrf_token() }}">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.0/css/ionicons.min.css" integrity="sha256-3iu9jgsy9TpTwXKb7bNQzqWekRX7pPK+2OLj3R922fo=" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.2/css/AdminLTE.min.css" integrity="sha256-sZVMQjkm0Ho60NDzV4Ot0OTBAK7UcaqkQOxLkTT93kQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.2/css/skins/_all-skins.min.css" integrity="sha256-U2eKBb7w142J6PSkrsgX1d3UM57TgiErTiT9gD1X8iA=" crossorigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     @yield('css')
   </head>
-  <body class="skin-green sidebar-mini" style="padding-right: 0px !important;">
-    <div class="wrapper" style="height: auto; min-height: 100%;" id="frontAminLTE">
+  <body class="skin-green sidebar-mini" style="height: auto; min-height: 100%;">
+    <div class="wrapper">
       @include('layout.recursos.header.index')
       @include('layout.recursos.left_menu.index')
       <div class="content-wrapper">
--- resources/views/layout/recursos/footer.blade.php
+++ resources/views/layout/recursos/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 1.1.1 &copy; 
+          <b>Version</b> 1.1.1 &copy;
           <b style="font-size:12px;">
-          	<?php 
-          		$hostname =explode('.cosapidata.pe',gethostname());
+          	<?php
+          		$hostname =explode('.azure.net',gethostname());
           		echo  $hostname[0];
           	?>
           </b>
         </div>
-        <strong>Copyright &copy; {{date('Y')}} <a href="http://www.sapia.com.pe" target="_blank">Sapia</a>.</strong> Derechos Reservados.
+        <strong>Copyright &copy; {{date('Y')}} <a href="http://www.batmanbar.com.pe" target="_blank">Bar Batman</a>.</strong> Derechos Reservados.
 </footer>
