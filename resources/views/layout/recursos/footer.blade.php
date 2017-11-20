af9f5f340f1bbf917dda50d61beabdffae161d06
Alan Cornejo <alancornejo@icloud.com>
2017-11-20 01:22:37 -0500

    Configurar Footer

---
+++ resources/views/layout/recursos/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 1.1.1 &copy; 
+          <b style="font-size:12px;">
+          	<?php 
+          		$hostname =explode('.cosapidata.pe',gethostname());
+          		echo  $hostname[0];
+          	?>
+          </b>
+        </div>
+        <strong>Copyright &copy; {{date('Y')}} <a href="http://www.sapia.com.pe" target="_blank">Sapia</a>.</strong> Derechos Reservados.
+</footer>
