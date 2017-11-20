030c9f6f1da95463043f339eadd1860dfa392f4a
Alan Cornejo <alancornejo@icloud.com>
2017-11-20 00:38:06 -0500

    Creacion del Header

---
+++ resources/views/layout/recursos/header/index.blade.php
@@ -0,0 +1,18 @@
+<header class="main-header">
+    <a href="/" class="logo">
+        <span class="logo-mini">Lic</span>
+        <span class="logo-lg">Licoreria</span>
+    </a>
+    <nav class="navbar navbar-static-top" role="navigation" id="menuHeader">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+              <li>
+                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+              </li>
+            </ul>
+        </div>
+    </nav>
+</header>
